a33e46b7467281cf04090aecadd45f03f19ba64e
cizel <i@cizel.cn>
2019-01-12 23:53:47 +0800

    feat: update brew.sh

--- script/brew.sh
+++ script/brew.sh
@@ -8,7 +8,6 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
-
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
 brew install moreutils
@@ -36,14 +35,49 @@ brew install hub
 brew install the_silver_searcher
 brew install fzf
 
-brew install git
+# System extra tools
 brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
+brew install gcc
+brew install cmake
+brew install openssl
+brew install automake
+brew install htop
+brew install nmap
+brew install telnet
+brew install trash
 brew install rename
 brew install tree
-brew install ffmpeg --with-libvpx
+brew install ffmpeg
 
+# Programing & Tools
+brew install git
+brew install node # This installs `npm` too using the recommended installation method
+brew install ruby
+brew install php
+brew install go
+brew install python
+brew install python@2
+brew install glide
+brew install yarn
+
+# useful tools
+brew install tig
 brew install ncdu # find where your diskspace went
+brew install tldr
+brew install bat
+brew install privoxy
+brew install proxychains-ng
+brew install caddy
+brew install hugo
+
+# edit
+brew install editorconfig
+brew install global
+brew install ctags
+brew install fcitx-remote-for-osx # https://github.com/CodeFalling/fcitx-remote-for-osx
+brew install reattach-to-user-namespace
+
+brew install tmux
 
 brew install zsh
 
